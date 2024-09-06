13d7a64f065c50477c6691f7728404a6ab2e49c5
Blumenkranz <gelq0912@gmail.com>
2024-09-07 00:36:27 +0800

    Fix the sign of Mu_L and Mu_R in moment_u_3D1F and moment_u_2D2F.

--- lib/KitCore/2D2F.jl
+++ lib/KitCore/2D2F.jl
@@ -101,51 +101,51 @@ function moment_u_2D2F(U::T,V::Real,λ::Real,n::Integer,m::Integer,K::Real) wher
     @inbounds begin
         Mu_L[1] = 0.5 * erfc(-√(λ) * U)
         Mu_L[2] =
-            U * Mu_L[1] -  0.5 * exp(-λ * U^2) / (√(π * λ))
-        Mu_R[1] = 0.5 * erfc(√(λ) * U)
-        Mu_R[2] =
-            U * Mu_R[1] + 0.5 * exp(-λ * U^2) / (√(π * λ))
-        for i = 1:n-1
-            Mu_L[i+2] = U * Mu_L[i+1] + 0.5 * i / λ * Mu_L[i]
-            Mu_R[i+2] = U * Mu_R[i+1] + 0.5 * i / λ * Mu_R[i]
-        end
-        Mu = @. Mu_L + Mu_R
-        Mv = Vector{T}(undef, m + 1)
-        Mv[1] = one(T)
-        Mv[2] = V
-        for i = 1:m-1
-            Mv[i+2] = V * Mv[i+1] + 0.5 * i * Mv[i] / λ
-        end
-        Mξ2 = 0.5 * K / (λ)
-        Mξ4 = (K^2 + 2.0 * K) / (4.0 * λ^2)
-        Mξ = [one(Mξ2), Mξ2, Mξ4]
-    end
-    return (Mu, Mv, Mξ, Mu_L, Mu_R)
-end
+            U * Mu_L[1] + 0.5 * exp(-λ * U^2) / (√(π * λ))
+		Mu_R[1] = 0.5 * erfc(√(λ) * U)
+		Mu_R[2] =
+		    U * Mu_R[1] - 0.5 * exp(-λ * U^2) / (√(π * λ))
+		for i = 1:n-1
+		    Mu_L[i+2] = U * Mu_L[i+1] + 0.5 * i / λ * Mu_L[i]
+		    Mu_R[i+2] = U * Mu_R[i+1] + 0.5 * i / λ * Mu_R[i]
+		end
+		Mu = @. Mu_L + Mu_R
+		Mv = Vector{T}(undef, m + 1)
+		Mv[1] = one(T)
+		Mv[2] = V
+		for i = 1:m-1
+		    Mv[i+2] = V * Mv[i+1] + 0.5 * i * Mv[i] / λ
+		end
+		Mξ2 = 0.5 * K / (λ)
+		Mξ4 = (K^2 + 2.0 * K) / (4.0 * λ^2)
+		Mξ = [one(Mξ2), Mξ2, Mξ4]
+	    end
+	    return (Mu, Mv, Mξ, Mu_L, Mu_R)
+	end
 
-function moment_uv_2D2F(Mu::AbstractVector{T}, Mv, Mξ, α::Integer, β::Integer, δ::Integer) where{T} # calc <u^αv^βξ^δψ>
-    Muv = Vector{T}(undef, 4)
-    @inbounds begin
-        Muv[1] = Mu[α+1] * Mv[β+1] * Mξ[Int(δ / 2 + 1)]
-        Muv[2] = Mu[α+2] * Mv[β+1] * Mξ[Int(δ / 2 + 1)]
-        Muv[3] = Mu[α+1] * Mv[β+2] * Mξ[Int(δ / 2 + 1)]
-        Muv[4] =
-            0.5 * (
-                Mu[α+3] * Mv[β+1] * Mξ[Int(δ / 2 + 1)] +
-                Mu[α+1] * Mv[β+3] * Mξ[Int(δ / 2 + 1)] +
-                Mu[α+1] * Mv[β+1] * Mξ[Int(δ / 2 + 2)]
-            )
-    end
-    return Muv
-end
+	function moment_uv_2D2F(Mu::AbstractVector{T}, Mv, Mξ, α::Integer, β::Integer, δ::Integer) where{T} # calc <u^αv^βξ^δψ>
+	    Muv = Vector{T}(undef, 4)
+	    @inbounds begin
+		Muv[1] = Mu[α+1] * Mv[β+1] * Mξ[Int(δ / 2 + 1)]
+		Muv[2] = Mu[α+2] * Mv[β+1] * Mξ[Int(δ / 2 + 1)]
+		Muv[3] = Mu[α+1] * Mv[β+2] * Mξ[Int(δ / 2 + 1)]
+		Muv[4] =
+		    0.5 * (
+			Mu[α+3] * Mv[β+1] * Mξ[Int(δ / 2 + 1)] +
+			Mu[α+1] * Mv[β+3] * Mξ[Int(δ / 2 + 1)] +
+			Mu[α+1] * Mv[β+1] * Mξ[Int(δ / 2 + 2)]
+		    )
+	    end
+	    return Muv
+	end
 
-function moment_au_2D2F(a, Mu, Mv, Mξ, α::Integer, β::Integer)# calc <au^αv^βψ> 
-    @inbounds a[1] * moment_uv_2D2F(Mu, Mv, Mξ, α, β, 0) +
-              a[2] * moment_uv_2D2F(Mu, Mv, Mξ, α + 1, β, 0) +
-              a[3] * moment_uv_2D2F(Mu, Mv, Mξ, α, β + 1, 0) +
-              0.5 * a[4] * moment_uv_2D2F(Mu, Mv, Mξ, α + 2, β, 0) +
-              0.5 * a[4] * moment_uv_2D2F(Mu, Mv, Mξ, α, β + 2, 0) +
-              0.5 * a[4] * moment_uv_2D2F(Mu, Mv, Mξ, α, β, 2)
-end
+	function moment_au_2D2F(a, Mu, Mv, Mξ, α::Integer, β::Integer)# calc <au^αv^βψ> 
+	    @inbounds a[1] * moment_uv_2D2F(Mu, Mv, Mξ, α, β, 0) +
+		      a[2] * moment_uv_2D2F(Mu, Mv, Mξ, α + 1, β, 0) +
+		      a[3] * moment_uv_2D2F(Mu, Mv, Mξ, α, β + 1, 0) +
+		      0.5 * a[4] * moment_uv_2D2F(Mu, Mv, Mξ, α + 2, β, 0) +
+		      0.5 * a[4] * moment_uv_2D2F(Mu, Mv, Mξ, α, β + 2, 0) +
+		      0.5 * a[4] * moment_uv_2D2F(Mu, Mv, Mξ, α, β, 2)
+	end
 
 
