9754069ae00a36d5302c4b0152ada7d2c24d954c
Blumenkranz <gelq0912@gmail.com>
2025-01-01 21:59:47 +0800

    Fix time marching bugs. It seems that there're stability problems for ::DVM type flux.

--- src/iterate.jl
+++ src/iterate.jl
@@ -55,21 +55,25 @@ function update_gradmax!(amr::AMR)
     global_data.status.gradmax =
         MPI.Allreduce(global_data.status.gradmax, max, MPI.COMM_WORLD)
 end
-function update_df!(
-    vs_data::VS_Data,
-    F::AbstractArray,
-    F_::AbstractArray,
-    τ::Real,
-    τ_::Real,
-    area::Real,
-    global_data::Global_Data,
-)
-    f = vs_data.df
-    Δt = global_data.status.Δt
-    @inbounds @. f = (f + 0.5 * Δt * (F / τ + (F_ - f) / τ_)) / (1.0 + 0.5 * Δt / τ)
-    @inbounds @. f += vs_data.flux / area / (1.0 + 0.5 * Δt / τ)
-end
+# function update_df!(
+#     vs_data::VS_Data,
+#     F::AbstractArray,
+#     F_::AbstractArray,
+#     τ::Real,
+#     τ_::Real,
+#     area::Real,
+#     global_data::Global_Data,
+# )
+#     f = vs_data.df
+#     Δt = global_data.status.Δt
+#     @inbounds @. f = (f + 0.5 * Δt * (F / τ + (F_ - f) / τ_)) / (1.0 + 0.5 * Δt / τ)
+#     @inbounds @. f += vs_data.flux / area / (1.0 + 0.5 * Δt / τ)
+# end
 function update_volume!(amr::AMR)
+    time_marching = amr.global_data.config.solver.time_marching
+    update_volume!(time_marching,amr)
+end
+function update_volume!(::UGKS_Marching,amr::AMR)
     global_data = amr.global_data
     gas = global_data.config.gas
     trees = amr.field.trees
@@ -82,12 +86,8 @@ function update_volume!(amr::AMR)
             prim_ = get_prim(ps_data, global_data)
             τ_ = get_τ(prim_, gas.μᵣ, gas.ω)
             area = reduce(*, ps_data.ds)
-            if isa(amr.global_data.config.solver.flux,MicroFlux)
-                ps_data.w = calc_w0(ps_data)
-            else
-                ps_data.w .+= ps_data.flux ./ area
-                ps_data.flux .= 0.0
-            end
+            ps_data.w .+= ps_data.flux ./ area
+            ps_data.flux .= 0.0
             ps_data.prim .= prim = get_prim(ps_data, global_data)
             τ = get_τ(prim, gas.μᵣ, gas.ω)
             ps_data.qf .= qf = calc_qf(vs_data, prim_)
@@ -97,7 +97,40 @@ function update_volume!(amr::AMR)
             F_ .+= F⁺
             F⁺ = shakhov_part(vs_data.midpoint, F, prim, qf, global_data)
             F .+= F⁺
-            update_df!(vs_data, F, F_, τ, τ_, area, global_data)
+            f = vs_data.df
+            Δt = global_data.status.Δt
+            @inbounds @. f = (f + 0.5 * Δt * (F / τ + (F_ - f) / τ_)) / (1.0 + 0.5 * Δt / τ)
+            @inbounds @. f += vs_data.flux / area / (1.0 + 0.5 * Δt / τ)
+            vs_data.flux .= 0.0
+        end
+    end
+end
+function update_volume!(::Euler,amr::AMR)
+    global_data = amr.global_data
+    gas = global_data.config.gas
+    trees = amr.field.trees
+    @inbounds for i in eachindex(trees.data)
+        @inbounds for j in eachindex(trees.data[i])
+            ps_data = trees.data[i][j]
+            isa(ps_data,InsideSolidData) && continue
+            ps_data.bound_enc<0 && continue
+            vs_data = ps_data.vs_data
+            area = reduce(*, ps_data.ds)
+            if isa(amr.global_data.config.solver.flux,MicroFlux)
+                ps_data.w = calc_w0(ps_data)
+            else
+                ps_data.w .+= ps_data.flux ./ area
+                ps_data.flux .= 0.0
+            end
+            ps_data.prim .= prim = get_prim(ps_data, global_data)
+            τ = get_τ(prim, gas.μᵣ, gas.ω)
+            ps_data.qf .= qf = calc_qf(vs_data, prim)
+            F = discrete_maxwell(vs_data.midpoint, prim, global_data)
+            F⁺ = shakhov_part(vs_data.midpoint, F, prim, qf, global_data)
+            F .+= F⁺
+            f = vs_data.df
+            Δt = global_data.status.Δt
+            f .= (τ-Δt)/τ*f+Δt/τ*F+1/area*vs_data.flux
             vs_data.flux .= 0.0
         end
     end
