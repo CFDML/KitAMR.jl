b8b6264043b8cbb20fb0cd348dd1341958b31942
Blumenkranz <gelq0912@gmail.com>
2025-01-11 22:42:05 +0800

    Remove unecessary dependency.

--- src/test_r2v.jl
+++ src/test_r2v.jl
@@ -1,6 +1,6 @@
 using KitAMR
 KitAMR.result2vtk("result2025-01-09_21-43","modified1_UGKS_vtk")
 KitAMR.result2vtk("result2025-01-09_18-04","test_UGKS_vtk")
-KitAMR.result2vtk("result2025-01-11_21-49","test_vs_vtk")
+KitAMR.result2vtk("result2025-01-11_22-34","test_vs_vtk")
 KitAMR.result2vtk("result2025-01-10_21-28","test_non-vs_vtk")
 KitAMR.result2vtk("result2025-01-11_21-29","test_non-adapt_vtk")
