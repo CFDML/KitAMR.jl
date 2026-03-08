f8b478526321416ded9c8b1ff11ee309543a7f59
Blumenkranz <gelq0912@gmail.com>
2026-03-08 17:05:16 +0800

    Add documetation. Update README.md.

---
+++ docs/make.jl
@@ -0,0 +1,12 @@
+using Documenter
+using KitAMR
+type_page = ["Configuration"=>"type_configure.md"]
+makedocs(;
+    sitename = "KitAMR.jl",
+    modules = [KitAMR],
+    pages = [
+        "Home"=>"index.md",
+        "Types"=>type_page
+        ],
+    checkdocs = :none
+)
---
+++ docs/src/type_configure.md
@@ -0,0 +1,9 @@
+# Configuration of problem
+
+To simulate a problem using KitAMR, user is required to define the problem. Most of the user-provided information is collected in `Configure` struct. 
+
+```@docs
+Configure
+```
+
+
